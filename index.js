92bc01e69838d8c00302746efd4b2197647947b4
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-02-17 10:23:13 -0500

    commit tree 1fcfe77c9223fc37f2b1a9c38a5f2621663e8188

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer = "lindsay"
+
+function upperCaseCustomerName(){
+ customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "not lindsay"
+}
+
+//error: Uncaught TypeError: Assignment to constant variable. 
+  //at changeLeastFavoriteCustomer
